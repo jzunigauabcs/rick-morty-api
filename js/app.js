6ed63e8dd2321bb092e907c27bab1331ad1f04b4
Jesus Antonio Zuñiga Arce <jzuniga@uabcs.mx>
2020-11-06 02:53:11 -0700

    Add search box and reload random page button

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 const BASE_URL = "https://rickandmortyapi.com/api/";
-const DELAY_MS = 2000;
+const DELAY_MS = 100;
 
-const randomPage = function () {
-	const MAX_PAGE = 34;
+const randomPage = function() {
+	const  MAX_PAGE = 34;
 	return Math.floor(Math.random() * MAX_PAGE + 1)
 }
 
@@ -24,60 +24,92 @@ const delayLoading = function () {
 	});	
 }
 
-const fetchByPage = function (page) {
-	return new Promise((resolve, response) => {
+const fetchByPage = function(page) {
+	return new Promise((resolve, reject) => 
 		fetch(`${BASE_URL}character/?page=${page}`)
-		.then(response => resolve(response.json()));	
-	})
+		.then(response => resolve(response.json())))
 }
 
-const makeCard = function(information) {
+const fetchByName = function(name) {
+	return new Promise((resolve, reject) => 
+		fetch(`${BASE_URL}character/?name=${name}`)
+		.then(response => resolve(response.json())))
+}
+
+const templateCard = function({name, image, gender, species, episode}) {
 	return `<div class="card">
 					<div class="card-img">
-						<img src="${information.image}" alt="">
+						<img src="${image}" alt="">
 					</div>
 					<div class="card-body">
-						<h3 class="card-title">${information.name}</h3>
+						<h3 class="card-title">${name}</h3>
 						<div class="card-information">
 							<ul>
-								<li><strong>Género: </strong>${information.gender}</li>
-								<li><strong>Especie: </strong>${information.species}</li>
-								<li><strong>Episodios: </strong>${information.episode.length}</li>
+								<li><strong>Género: </strong>${gender}</li>
+								<li><strong>Especie: </strong>${species}</li>
+								<li><strong>Episodios: </strong>${episode}</li>
 							</ul>
 						</div>
 					</div>
 				</div>`
 }
 
-const loadCards = function (information) {
+const loadCards = function(results) {
 	let childs = "";
-	information.forEach(i => {
-		childs += makeCard(i)	
+	results.forEach(r => {
+		const cardInformation = {
+			name: r.name,
+			image: r.image,
+			gender: r.gender,
+			species: r.species,
+			episode: r.episode.length
+		}
+		childs += templateCard(cardInformation)
 	})
 	return childs;
 }
 
-const render = function(fn) {
+const render = function(callback) {
 	const cardsContainer = document.querySelector(".cards");
 	cardsContainer.innerHTML = "";
 	
-	toggleLoading()
+	toggleLoading();
 	delayLoading()
-	.then(() => {
-		return fn()
+	.then(function() {
+		return callback()
 	})
 	.then(response => {
-		const {results} = response;
-		console.log(results);
-		toggleLoading();
+		const {results} = response
+		toggleLoading()
 		cardsContainer.innerHTML = loadCards(results)
+	})
+	.catch(function() {
+		console.error("Ocurrió un error")
 	})
 	
 }
 
-const init = function () {
+const renderByRandomPage = function () {
 	const page = randomPage();
-	render(function(){return fetchByPage(page)})
+	render(function() { return fetchByPage(page)})
+}
+const renderByName = function (name) {
+	render(function() { return fetchByName(name)})
+}
+
+
+const init = function () {
+	renderByRandomPage();
+	const btnReload = document.querySelector("#btnReload");
+	btnReload.addEventListener('click', () => {
+		renderByRandomPage();
+	})
+	const formSearch = document.querySelector("#formSearch");
+	formSearch.addEventListener('submit', (e) => {
+		e.preventDefault();
+		const name = document.querySelector("#searchBox").value;
+		renderByName(name);
+	})
 }
 
 init();
